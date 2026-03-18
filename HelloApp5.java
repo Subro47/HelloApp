71612f4e8664330426c97628e00934cbf5bd6399
Subro47 <subhrojithati1612@gmail.com>
2026-03-18 14:19:39 +0530

    UC5

---
+++ HelloApp5.java
@@ -0,0 +1,14 @@
+public class HelloApp5 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for-loop (for-each)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+
+    }
+}
